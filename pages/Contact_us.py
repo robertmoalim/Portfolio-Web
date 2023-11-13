30079253c691bb3335dbe18e883c84ffaac79d34
robertmoalim <Robertbusiness0902@Hotmail.com>
2023-11-14 00:02:19 +0100

    Password protected + Contact Form Deployed -> Fixed

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -1,12 +1,23 @@
 import streamlit as st
+from send_email import send_email
+import pandas
 
-st.header("Contact Me")
+df = pandas.read_csv("topics.csv")
 
-with st.form(key="email_forms"):
-    user_email = st.text_input("Your email address")
-    message = st.text_area("Your message")
+with st.form(key="email_form"):
+    user_email = st.text_input("Your Email Address")
+    option = st.selectbox(
+        'What topic do you want to discuss?',
+        df["topic"])
+    raw_message = st.text_area("Text")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+Topic {option}
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        message = message + user_email
-
-        print("I was pressed!")
+        send_email(message)
+        st.info("Your email was sent successfully")
--- send_email.py
+++ send_email.py
@@ -1,17 +1,16 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "minionrobi@gmail.com"
-    password = ""
+    password = os.getenv("PASSWORD")
 
     receiver = "minionrobi@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
         server.login(username, password)
         server.sendmail(username, receiver, message)
