a72c6d09c760d789fd2016921e024052be2e6d20
robertmoalim <Robertbusiness0902@Hotmail.com>
2023-11-13 13:22:24 +0100

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo1.png", width=600)
+
+with col2:
+    st.title("Robert Moalim")
+    content = """
+    Hi, I am Robert! I am a inspiring Python programmer. I graduated in 2020 in International Business & Trade and I have done diverse programming courses.
+    I have worked with ........., such as,............ If you have any questions about me feel free to contact me!
+    """
+    st.info(content)
