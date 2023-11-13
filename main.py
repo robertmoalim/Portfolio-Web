ffe41b19ca2f994bb215a898ac2f124eb7721170
robertmoalim <Robertbusiness0902@Hotmail.com>
2023-11-13 13:28:39 +0100

    Add description on the new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo1.png", width=600)
+    st.image("images/photo1.png", width=200)
 
 with col2:
     st.title("Robert Moalim")
@@ -14,3 +14,8 @@ with col2:
     I have worked with ........., such as,............ If you have any questions about me feel free to contact me!
     """
     st.info(content)
+
+    content2 = """
+    Below you can find some of the apps I have built in Python. Feel free to contact me!
+    """
+    st.write(content2)
